20094dee98d46bfb59b0b516c6d5136d18bfec75
Pavel Sanchez <23495830+PaleBluDot@users.noreply.github.com>
2021-10-02 18:29:19 -0400

    feat: add chalk coloring

--- name-cli/index.js
+++ name-cli/index.js
@@ -2,23 +2,34 @@
 
 const pkg = require("./package.json");
 const welcome = require("cli-welcome");
+const chalk = require("chalk");
+const log = console.log;
+
+const error = chalk.bold.red;
+const warning = chalk.keyword("orange");
+const success = chalk.keyword("green");
+const info = chalk.keyword("blue");
 
 welcome({
-	title: pkg.name,
+	title: `Pavel Sanchez`,
 	tagLine: "Learn some more",
 	description: pkg.description,
 	version: pkg.version,
 	bgColor: "#0055aa",
-	Color: "#FADC00",
+	Color: "#ffff00",
 	bold: true,
 	clear: true,
 });
 
-console.log(`
-Pavel Sanchez
+log(`
+${chalk.italic.whiteBright(
+	`Experienced digital marketer working in non-profit. Skilled in HTML emails, front-end web development, and UI design.`
+)}
 
-Experienced digital marketer working in non-profit. Skilled in HTML emails, front-end web development, and UI design.
-
-🏠 Website: https://pavelsanchez.com
-📖 Github: https://github.com/PaleBluDot
+${chalk.bgYellow.hex("#121212")(" Website: ")}${chalk.hex("#a6a6a6")(
+	` https://pavelsanchez.com `
+)}
+${chalk.bgYellow.hex("#121212")(" Github:  ")}${chalk.hex("#a6a6a6")(
+	` https://github.com/PaleBluDot `
+)}
 `);
