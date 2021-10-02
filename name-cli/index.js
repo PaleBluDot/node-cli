0960d75f55d86ad66205bc83dd968a3b22e2ae9f
Pavel Sanchez <23495830+PaleBluDot@users.noreply.github.com>
2021-10-02 13:27:19 -0400

    initial commit

---
+++ name-cli/index.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+console.log(`
+Pavel Sanchez - Front-end Engineer
+
+Experienced digital marketer working in non-profit. Skilled in HTML emails, front-end web development, and UI design.
+
+🏠 Website: https://pavelsanchez.com
+📖 Github: https://github.com/PaleBluDot`);
